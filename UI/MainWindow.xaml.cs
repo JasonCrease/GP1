b4241307725db840d99b767a36a014c38ef54105
Jason Crease <jason.crease@red-gate.com>
2015-09-02 16:59:13 +0100

    Removed crappy functions

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -27,38 +27,17 @@ namespace UI
         {
             InitializeComponent();
             m_Engine = new Engine();
-            m_Engine.FitnessFunction = new FitnessFunctionAlternatesGettingLarger();
+            m_Engine.FitnessFunction = new FitnessFunctionCards();
         }
 
         private Program m_Program1;
         private Program m_Program2;
         private Engine m_Engine;
-
-        private void buttonSearchRandomly_Click(object sender, RoutedEventArgs e)
-        {
-            float bestFitnessSoFar = 1000f;
-            int maxTrials = 100000;
-            int trials = 0;
-
-            while (trials++ < maxTrials && bestFitnessSoFar > 0f)
-            {
-                Program program = m_Engine.CreateRandomProgram();
-                float fitness = m_Engine.FitnessFunction.Evaluate(program);
-                if (fitness < bestFitnessSoFar)
-                {
-                    m_Program1 = program;
-                    bestFitnessSoFar = fitness;
-                }
-            }
-
-            DrawProgram(m_Program1, imageProgram1);
-            ShowFitness(m_Program1);
-            GP1.Compiler.Compiler compiler = new GP1.Compiler.Compiler();
-            compiler.Compile(m_Program1, "Prog.dll");
-        }
-
+        
         public void EvolutionDone(object sender, EventArgs e)
         {
+            updateUiTimer.Dispose();
+
             Program p = m_Engine.GetStrongestProgram();
             GP1.Compiler.Compiler compiler = new GP1.Compiler.Compiler();
             compiler.Compile(p, "Prog.dll");
@@ -84,40 +63,10 @@ namespace UI
                 }
             );
         }
-
-        private void buttonGenRandProgram1_Click(object sender, RoutedEventArgs e)
-        {
-            Program program = m_Engine.CreateRandomProgram();
-            DrawProgram(program, imageProgram1);
-            ShowFitness(program);
-            m_Program1 = program;
-
-            GP1.Compiler.Compiler compiler = new GP1.Compiler.Compiler();
-            compiler.Compile(m_Program1, "Prog.dll");
-        }
-        private void buttonGenRandProgram2_Click(object sender, RoutedEventArgs e)
-        {
-            Program program = m_Engine.CreateRandomProgram();
-            DrawProgram(program, imageProgram2);
-            ShowFitness(program);
-            m_Program2 = program;
-        }
-        private void buttonShowCrossoverProgram_Click(object sender, RoutedEventArgs e)
-        {
-            Program program = m_Program1.Crossover(m_Program2);
-            DrawProgram(program, imageProgram3);
-            ShowFitness(program);
-        }
-        private void buttonMutateProgram_Click(object sender, RoutedEventArgs e)
-        {
-            m_Program1.Mutate();
-            DrawProgram(m_Program1, imageProgram3);
-            ShowFitness(m_Program1);
-        }
-
+        
         private void ShowFitness(Program program)
         {
-            labelFitness.Content = m_Engine.FitnessFunction.Evaluate(program).ToString("0.00");
+            labelFitness.Content = program.Fitness.ToString("0.00");
         }
 
         private void DrawProgram(Program program, System.Windows.Controls.Image image)

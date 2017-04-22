a132909a233991783456db6a66f8bc57bfee24e5
Jason Crease <jason.crease@red-gate.com>
2017-04-22 16:47:31 +0100

    Generate 300 hands for 2-card fitness function rather than hard-coded

--- GP1/FitnessFunction2CardPoker.cs
+++ GP1/FitnessFunction2CardPoker.cs
@@ -1,4 +1,6 @@
-﻿namespace GP1
+﻿using System;
+
+namespace GP1
 {
     public class FitnessFunction2CardPoker : IFitnessFunction
     {
@@ -10,49 +12,57 @@
             }
         }
 
+        const int HANDSTODEAL = 300;
+        int[][] C = new int[HANDSTODEAL][];
+
+        public FitnessFunction2CardPoker()
+        {
+            int EACHTODEAL = HANDSTODEAL / 3;
+            Random r = new Random(11);
+
+            int straightsAdded = 0;
+            int twoKindAdded = 0;
+            int unknownAdded = 0;
+            int i = 0;
+
+            while (i < HANDSTODEAL - 1)
+            {
+                i = straightsAdded + twoKindAdded + unknownAdded;
+                C[i] = new int[3];
+                int card1 = C[i][1] = r.Next(1, 13);
+                int card2 = C[i][2] = r.Next(1, 13);
+
+                Array.Sort(C[i]);
+
+                if (C[i][1] + 1 == C[i][2] && straightsAdded < EACHTODEAL)  //straight
+                {
+                    C[i][0] = 2;
+                    straightsAdded++;
+                }
+                else if (C[i][1] == C[i][2] && twoKindAdded < EACHTODEAL)  // 2 of a kind
+                {
+                    C[i][0] = 1;
+                    twoKindAdded++;
+                }
+                else if (unknownAdded < EACHTODEAL)
+                {
+                    C[i][0] = 0;
+                    unknownAdded++;
+                }
+            }
+
+        }
+
         public double Evaluate(Program program)
         {
             double fitness = 0.00;
 
-            int[,] C = new int[,]
+            for (int i = 0; i < HANDSTODEAL; i += 1)
             {
-                // Nothing
-                {0,     1, 4},
-                {0,     1, 8},
-                {0,     2, 4},
-                {0,     2, 9},
-                {0,     4, 6},
-                {0,     6, 9},
-                {0,     7, 9},
-                {0,     9, 12},
-
-                // Pair
-                {1,     2, 2},
-                {1,     3, 3},
-                {1,     4, 4},
-                {1,     5, 5},
-                {1,     6, 6},
-                {1,     7, 7},
-                {1,     9, 9},
-                {1,     12, 12},
-
-                // Straight
-                {2,     1, 2},
-                {2,     2, 3},
-                {2,     3, 4},
-                {2,     4, 5},
-                {2,     5, 6},
-                {2,     6, 7},
-                {2,     7, 8},
-                {2,     11, 12},
-            };
-
-            for (int i = 0; i < C.Length / 3; i += 1)
-            {
-                program.Variables[0].Value = C[i, 1];
-                program.Variables[1].Value = C[i, 2];
+                program.Variables[0].Value = C[i][1];
+                program.Variables[1].Value = C[i][2];
                 program.Run();
-                float desiredResult = C[i, 0];
+                float desiredResult = C[i][0];
                 fitness += program.Result == desiredResult ? 0 : 1;
             }
 
